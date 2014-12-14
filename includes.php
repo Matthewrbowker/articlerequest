fa3814776ae872369812843575e1361a2ed21c4b
Matthew Bowker <matthewrbowker@me.com>
2014-12-14 00:54:22 -0700

    Convert to markdown readme, fix UA string bug

--- includes.php
+++ includes.php
@@ -1,10 +1,5 @@
 <?php
 
-// Set the UA string
-ini_set('user_agent', 
-  'Article_Request_Tool - Matthewrbowker - Version 0.1devel1');
-  
-
 // Require all the files we need.
 require('vars/config.php');
 require('vars/config.inc.php');
@@ -15,4 +10,7 @@ require('vars/translate.php');
 require('vars/site.php');
 require('vars/pdo.php');
 
+// Set the UA string
+ini_set('user_agent', 
+  "Article_Request_Tool - Matthewrbowker - {$version}");
 ?>
