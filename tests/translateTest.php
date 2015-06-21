a914719657a7073c178f364f90c6e3eae1b540b3
Matthew Bowker <matthewrbowker@me.com>
2015-06-21 08:13:35 -0600

    Basic framework for phpunit

---
+++ tests/translateTest.php
@@ -0,0 +1,13 @@
+<?php
+
+class TranslateTest extends PHPUnit_Framework_TestCase
+{
+    public function testAdd()
+    {
+        // $calculator = new vars\translate;
+
+        // $result = $calculator->add(3, 4);
+
+        $this->assertEquals(0, 0);
+    }
+}
