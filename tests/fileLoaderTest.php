f2abfeaab789693ba8da0b890640428464de85cf
Matthew Bowker <matthewrbowker@me.com>
2015-10-22 15:27:29 -0600

    Add tests

--- tests/fileLoaderTest.php
+++ tests/fileLoaderTest.php
@@ -21,4 +21,19 @@ class fileLoaderTest extends PHPUnit_Framework_TestCase
         $this->fl = NULL;
     }
 
+    public function checkMatches() {
+
+        $this->assertEquals($this->fl->_r("test1"), "TEST 1");
+        $this->assertEquals($this->fl->_r("test2"), "TEST 2");
+        $this->assertEquals($this->fl->_r("test3"), "TEST 3");
+        $this->assertEquals($this->fl->_r("test4"), NULL);
+
+        // Testing they return the wrong values
+        $this->assertNotEquals($this->fl->_r("test3"), "TEST 1");
+        $this->assertNotEquals($this->fl->_r("test1"), "TEST 2");
+        $this->assertNotEquals($this->fl->_r("test2"), "TEST 3");
+        $this->assertNotEquals($this->fl->_r("test4"), "TEST 1");
+
+    }
+
 }
